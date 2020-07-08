c555a1f78ca5b71aed4aa3b25bfdf29625c4457d
Martin Spusta <lucas.spusta@gmail.com>
2020-07-08 18:52:37 -0400

    Added Action Sheet

--- SwiftUICameraApp/ContentView.swift
+++ SwiftUICameraApp/ContentView.swift
@@ -33,27 +33,69 @@ extension CaptureImageView: UIViewControllerRepresentable {
   }
 }
 
+struct ExisitingCaptureImageView {
+  
+  /// MARK: - Properties
+  @Binding var isShown: Bool
+  @Binding var image: Image?
+
+  func makeCoordinator() -> Coordinator {
+    return Coordinator(isShown: $isShown, image: $image)
+  }
+}
+
+extension ExisitingCaptureImageView: UIViewControllerRepresentable {
+  func makeUIViewController(context: UIViewControllerRepresentableContext<ExisitingCaptureImageView>) -> UIImagePickerController {
+    let picker = UIImagePickerController()
+    picker.delegate = context.coordinator
+    /// Default is images gallery. Un-comment the next line of code if you would like to test camera
+    //picker.sourceType = .camera
+    return picker
+  }
+  
+  func updateUIViewController(_ uiViewController: UIImagePickerController,
+                              context: UIViewControllerRepresentableContext<ExisitingCaptureImageView>) {
+    
+  }
+}
+
+struct GradientButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .foregroundColor(Color.white)
+            .padding()
+            .background(LinearGradient(gradient: Gradient(colors: [Color.blue, Color.green]), startPoint: .leading, endPoint: .trailing))
+            .cornerRadius(5.0)
+            .scaleEffect(configuration.isPressed ? 1.3 : 1.0)
+          
+    }
+}
+
 struct ContentView: View {
     @State var image: Image? = nil
-    @State var showCaptureImageView: Bool = false
+    @State var showCaptureImage: Bool = false
+    @State var showCaptureExistingImage: Bool = false
     @State private var showingSheet = false
     
     var body: some View {
         ZStack {
           VStack {
             Button(action: {
-              //self.showCaptureImageView.toggle()
                 self.showingSheet = true
             }) {
               Text("Open Pop-Up")
-            }
+            }.buttonStyle(GradientButtonStyle())
+          
             .actionSheet(isPresented: $showingSheet) {
                    ActionSheet(title: Text("What do you want to do?"), message: Text("There's only one choice..."), buttons: [
                             .default(Text("Take Photo")){
-                                self.showCaptureImageView.toggle()
+                                self.showCaptureImage.toggle()
                             },
                             .default(Text("Choose Photo")){
-                                self.showCaptureImageView.toggle()
+                                self.showCaptureExistingImage.toggle()
+                            },
+                            .default(Text("Capture Video")){
+                               
                             },
                             .cancel(Text("Cancel"))
                    ]
@@ -62,12 +104,15 @@ struct ContentView: View {
             image?.resizable()
               .frame(width: 250, height: 200)
               .clipShape(Circle())
-              .overlay(Circle().stroke(Color.white, lineWidth: 4))
               .shadow(radius: 10)
           }
-          if (showCaptureImageView) {
-            CaptureImageView(isShown: $showCaptureImageView, image: $image)
-          }
+           if (showCaptureImage) {
+             CaptureImageView(isShown: $showCaptureImage, image: $image)
+           }
+            
+            if (showCaptureExistingImage) {
+                ExisitingCaptureImageView(isShown: $showCaptureExistingImage, image: $image)
+            }
         }
       }
 
